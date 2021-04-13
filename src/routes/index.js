0b787149a2ca43da5414b49d8a65ba14c3fff14c
santiago <santiago.ottolini97@hotmail.com>
2021-04-13 09:41:05 -0300

    Firebase: Conection with Firebase

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,20 @@
 const { Router } = require("express");
 const router = Router();
+const admin = require("firebase-admin");
+
+//Conection with Firebase
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://fir-node-9df46-default-rtdb.firebaseio.com/",
+});
 
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+router.post("/new-contact", (req, res) => {
+  console.log(req.body);
+  res.send("Recived");
 });
 
 module.exports = router;
