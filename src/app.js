ba55ef7f27450b12802f21948694c770e56a5eb6
santiago <santiago.ottolini97@hotmail.com>
2021-04-13 08:48:57 -0300

    Firebase: Configuration server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const expresshdb = require('express-handlebars');
+const path = require('path');
 
-module.exports =app
+const app = express();
+
+//SETTING
+//process.env.PORT = validation, if the port not exist assign a port, in this case 3000
+app.set('port', process.env.PORT || 3000); 
+//path.join, allow to combine two routes fodlers
+//__dirname said where the folder src is ,and the second parameters is view folder
+app.set("view", path.join(__dirname, "view"))
+app.engine(".hbs", expresshdb({
+    //Thats mean that one files share the same configuration
+    defaultLayout="main",
+    //Write the extencion that use the file syste
+    extname=".hbs"
+}))
+app.set("view engine", ".hbs")
+
+//middleware
+
+//routes
+
+//static files
+
+module.exports = app;
