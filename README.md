3a7f375788bf5186a02edc3edf8196e998e7bdca
santiago <santiago.ottolini97@hotmail.com>
2021-04-13 10:16:34 -0300

    Firebase: POST with Firebase

---
+++ README.md
@@ -0,0 +1,1 @@
+$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\Santiago\Desktop\Firebase\fir-node-9df46-firebase-adminsdk-u2i74-c6cbdafbca.json"
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,19 +2,34 @@ const { Router } = require("express");
 const router = Router();
 const admin = require("firebase-admin");
 
+var serviceAccount = require("../../fir-node-9df46-firebase-adminsdk-u2i74-c6cbdafbca.json");
 //Conection with Firebase
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fir-node-9df46-default-rtdb.firebaseio.com/",
 });
 
+//it's like saying, of all the service i wanna connect to the database
+const db = admin.database();
+
 router.get("/", (req, res) => {
-  res.render("index");
+  //Bring all the data of the table contacts
+  db.ref("contacts").once("value", (snapshot) => {
+    const data = snapshot.val();
+    res.render("index", { contacts: data });
+  });
 });
 
 router.post("/new-contact", (req, res) => {
-  console.log(req.body);
-  res.send("Recived");
+  const newContact = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+  //ref, with what name i gonna save. Contacts in SQL is the table and in mongo is the collection
+  db.ref("contacts").push(newContact);
+  res.redirect("/");
 });
 
 module.exports = router;
