b278187191e1b5ed1194bac7e65c0282b0701416
santiago <santiago.ottolini97@hotmail.com>
2021-04-13 09:11:55 -0300

    Firebase: Start routes and views

--- src/app.js
+++ src/app.js
@@ -10,24 +10,27 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 //path.join, allow to combine two routes fodlers
 //__dirname said where the folder src is ,and the second parameters is view folder
-app.set("view", path.join(__dirname, "view"))
-app.engine(".hbs", expresshdb({
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  expresshdb({
     //Thats mean that one files share the same configuration
-    defaultLayout="main",
+    defaultLayout: "main",
     //Write the extencion that use the file syste
-    extname=".hbs"
-}))
-app.set("view engine", ".hbs")
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
 
 //MIDLEWARE
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 //When we have a forms, with this line can accepts this forms html, extended false said that only
 //recive json format, not image or video for instance
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
 //ROUTES
-
+app.use(require("./routes/index"));
 //STATIC FILES
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
